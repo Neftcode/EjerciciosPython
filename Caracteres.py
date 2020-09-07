6e258eeba8721d4b430765b856fb53485aaa3088
Neftcode <arevalocdev@outlook.com>
2020-09-07 07:13:20 -0500

    Problema 3 Caracteres

---
+++ Caracteres.py
@@ -0,0 +1,6 @@
+'''
+    Problema 3
+    @author Carlos Arévalo
+'''
+cadena = input("Ingrese una cadena de caracteres: ")
+print(f"El primer caracter es: {cadena[0]}\nEl último caracter es: {cadena[len(cadena)-1]}")
