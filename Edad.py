06460dd62813b8c72e1ea3dee413badc485382b1
Neftcode <arevalocdev@outlook.com>
2020-09-07 06:48:17 -0500

    Problema 1

---
+++ Edad.py
@@ -0,0 +1,9 @@
+'''
+    Problema 1
+    @author Carlos Arévalo
+'''
+edad = int(input("Digite su edad: "))
+anio = 2020
+diffAnios = 2070-anio
+edad2070 = edad+diffAnios
+print(f"Su edad en el año 2070 será de: {edad2070} años")
--- HolaMundo.py
+++
@@ -1,6 +0,0 @@
-'''
-    Problema 1
-    @author Carlos Arévalo
-'''
-edad = int(input("Digite su edad: "))
-print(f"Su edad es {edad}")
