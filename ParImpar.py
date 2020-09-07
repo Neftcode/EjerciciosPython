d19bc5499f8bc95e108f092c7b3859efa216dea8
Neftcode <arevalocdev@outlook.com>
2020-09-07 06:58:59 -0500

    Problema 2 ParImpar

---
+++ ParImpar.py
@@ -0,0 +1,9 @@
+'''
+    Problema 2
+    @author Carlos Arévalo
+'''
+numero = int(input("Ingrese un número entero: "))
+if numero % 2 == 0:
+    print("Número par")
+else:
+    print("Número impar")
